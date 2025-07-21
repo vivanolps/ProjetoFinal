eb2fe706a8c34d3b2b57fae95257aa978346d562
Evelyn Carvalho <vinhahh.3000@gmail.com>
2025-07-21 21:39:20 +0000

    Melhorando o site.

---
+++ src/app/pages/aluno/suporte/suporte.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-suporte',
+  imports: [],
+  templateUrl: './suporte.component.html',
+  styleUrl: './suporte.component.scss'
+})
+export class SuporteComponent {
+
+}
